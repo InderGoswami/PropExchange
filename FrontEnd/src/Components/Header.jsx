a6c395f4668695bfc56aabf9e2e8b74293dd1ea0
InderGoswami <indergoswami0111@gmail.com>
2024-12-31 17:04:49 +0530

    Header search is updated

--- FrontEnd/src/Components/Header.jsx
+++ FrontEnd/src/Components/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { Link,useNavigate } from 'react-router-dom';
 import logo from "F:/Projects/PropExchange/FrontEnd/src/assets/PropExchange.png";
 import { useSelector } from 'react-redux';
-
+import {FaSearch} from 'react-icons/fa'
 const Header = () => {
+  const navigate=useNavigate();
   const { currentUser } = useSelector(state => state.user);
+  const [searchTerm,setSearchTerm]=useState('');
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const urlParams=new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm',searchTerm);
+    const searchQuery=urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+  useEffect(()=>{
+    const urlParams=new URLSearchParams(location.search);
+    const searchTermFromUrl=urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+    }
 
+  },[location.search])
   return (
     <header className=" bg-[#7AB2D3] shadow-md fixed top-0 left-0 w-full h-16 z-50">
       <div className="h-full flex flex-col md:flex-row justify-between items-center max-w-6xl mx-auto px-4">
@@ -19,12 +36,17 @@ const Header = () => {
         </div>
 
         {/* Search Bar */}
-        <form className="w-full md:w-2/5 flex justify-center md:justify-end items-center mt-2 md:mt-0">
+        <form onSubmit={handleSubmit} className="w-full md:w-2/5 flex justify-center md:justify-end items-center mt-2 md:mt-0">
           <input
             type="text"
             placeholder="Search"
             className="w-4/5 md:w-full max-w-[250px] p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
           />
+          <button>
+            <FaSearch></FaSearch>
+          </button>
         </form>
 
         {/* Navigation */}
